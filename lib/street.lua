96e4faca30e59daa34f9993942c10507506a6e20
Eshed <ashdnazg@gmail.com>
2017-04-23 17:38:06 +0300

    Rudimentary street

--- lib/street.lua
+++ lib/street.lua
@@ -8,27 +8,59 @@ function Street:initialize(advanceTo, timer)
 	self.advanceTo = advanceTo
 	self.timer = timer
 	self.newGame = true
+	self.bg = love.graphics.newImage('assets/img/background.png')
+	self.bar = love.graphics.newImage("assets/img/options_bar.png")
+
+
+	self.frames = {
+		stand = love.graphics.newImage('assets/img/street/man_stand_tap.png'),
+		left = love.graphics.newImage('assets/img/street/man_left.png'),
+		right = love.graphics.newImage('assets/img/street/man_right.png'),
+		mad = love.graphics.newImage('assets/img/street/man_mad.png'),
+	}
 end
+
+function Street:reset()
+	self.newGame = false
+	self.timer:reset()
+	self.xPos = 0
+	self.yPos = 70
+	self.frame = 'stand'
+	self.frameTime = 0
+end
+
 
 function Street:start()
 	if self.newGame then
-		self.newGame = false
-		self.timer:reset()
+		self:reset()
 	end
 end
 
 
 function Street:draw()
 	love.graphics.print('street',0,0)
+	love.graphics.draw(self.bg)
+	love.graphics.draw(self.bar,0,300)
+	love.graphics.draw(self.frames[self.frame],self.xPos,self.yPos)
 	self.timer:draw_timer()
+	if self.frameTime <= 0 and self.frame ~= 'stand' then
+		self.frame = 'stand'
+	end
 end
 
 function Street:update(dt)
+	self.frameTime = self.frameTime - dt
 	self.timer:count_time(dt)
 end
 
 function Street:keyPress(key)
-	self.advanceTo('game')
+	if key == 'right' then
+		self.xPos = self.xPos + 12
+		self.frame = self.lastFrame == 'left' and 'right' or 'left'
+		self.lastFrame = self.frame
+		self.frameTime = 0.1
+	end
+	--self.advanceTo('game')
 end
 
 function Street:mousePressed(x, y, button)
